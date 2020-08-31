046f53dfa8941b8b37c5a2935617941124e49c06
Pat  Pignataro <pat6284@msn.com>
2020-08-31 17:44:05 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let taskList = document.getElementById('tasks');
 
-  document.addEventListener('submit', function(e) {
-    e.preventDefault()
-    let newTask = document.getElementById('new-task-description');
-    let li = document.createElement('li');
-    li.appendChild(document.createTextNode(newTask.value));
-    taskList.appendChild(li);
-    e.target.reset();
-  })
 });
